de14824b3c6f7930d45459eff984dd13ec885ee2
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-06-05 00:03:46 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,10 +21,9 @@ function Projects() {
             <ProjectCard
               imgPath={Business}
               isBlog={false}
-              title="Business Website"
-              description="A business website showcasing services, portfolio, and team to attract clients and share company information."
-              ghLink="https://github.com/mayank1365/Business_Website"
-              demoLink="https://mayank1365.github.io/Hackathon-Project-1/#"
+              title="Event Management System"
+              description="An end-to-end Event Management System using Spring Boot, MySQL, Hibernate ORMs, etc. for hosting + managing events."
+              ghLink="https://github.com/harshit4311"
             />
           </Col>
 
@@ -34,8 +33,7 @@ function Projects() {
                 isBlog={false}
                 title="Quiz App"
                 description="A quiz app featuring 10 general knowledge questions, designed to test your knowledge. Built with React.js, the app tracks your score, provides instant feedback on each answer, and saves your progress. "
-                ghLink="https://github.com/mayank1365/Quiz-App-Assignment-1"
-                demoLink="https://mayank1365.github.io/Quiz-App-Assignment-1/index.html"
+                ghLink="https://github.com/harshit4311"
             />
           </Col>
 
@@ -43,10 +41,9 @@ function Projects() {
             <ProjectCard
               imgPath={form}
               isBlog={false}
-              title="Form Validator"
-              description="A simple form validator built with JavaScript. Validates user inputs in real-time, ensuring data accuracy and integrity."
-              ghLink="https://github.com/mayank1365/Form-Validator-project"
-              demoLink="https://mayank1365.github.io/Form-Validator-project/"
+              title="Dog Breed Clasifier"
+              description="A Deep Learning Model using Efficient Net & ConvNeXt architectures for classifying Dog Breeds"
+              ghLink="https://github.com/harshit4311"
             />
           </Col>
 
@@ -57,3 +54,4 @@ function Projects() {
 }
 
 export default Projects;
+
