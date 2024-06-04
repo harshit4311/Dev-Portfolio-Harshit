27779e9acfa287437b53dd2485f697f1d2e398bd
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-06-05 00:07:20 +0530

    Update Github.js

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -6,7 +6,7 @@ function Github() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        My Github<strong className="purple">Commit Calendar</strong>
+        My Github <strong className="purple">Commit Calendar</strong>
       </h1>
       <GitHubCalendar
         username="harshit4311"
